6244c766e6c3592d14f19a72d2ecd4a36d8418e8
Martin Yanev <63072006+martinyanev94@users.noreply.github.com>
2024-09-28 17:07:11 -0400

    Add files via upload

---
+++ YGame_Sball/src/com/Area.java
@@ -0,0 +1,22 @@
+package com;
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+public class Area extends Actor {
+	
+	public Area(int x, int y) {
+		super(x, y);
+		
+		initArea();
+	}
+	
+	private void initArea() {
+	
+	ImageIcon icon = new ImageIcon("src/resources/area.png");
+	Image image = icon.getImage();
+	setImage(image);
+	}
+
+}
---
+++ YGame_Sball/src/com/Player.java
@@ -0,0 +1,32 @@
+package com;
+
+import java.awt.Image;
+
+import javax.swing.ImageIcon;
+
+public class Player extends Actor {
+	
+	public Player(int x, int y) {
+		super(x, y);
+		
+		initPlayer();
+		}
+	
+	private void initPlayer() {
+		
+		ImageIcon icon = new ImageIcon("src/resources/actor.png");
+		Image image = icon.getImage();
+		setImage(image);
+	}
+	
+	public void move(int x, int y) {
+
+        int dx = x() + x;
+        int dy = y() + y;
+        
+        setX(dx);
+        setY(dy);
+		
+	}	
+
+}
